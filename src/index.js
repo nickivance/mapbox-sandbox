dde8f18c60bdee6bd3ebfe5b1251ea1453a54a74
Nicki Vance <nicki@boxingindustries.com>
2021-10-13 18:46:11 -0700

    Revert "WIP on rotating maps."
    This reverts commit 2380959f64d17b7af2d7b08c299d5ffe0574b177.

--- src/index.js
+++ src/index.js
@@ -60,17 +60,6 @@ const TopRadio = ({map, name, onChange}) => {
 	);
 };
 
-const findCenter = ({top, left, height, width}) => {
-	let x = left + Math.round(width/2);
-	let y = top + Math.round(height/2);
-	return {x, y};
-};
-
-const findAngle = (center, mousePosition) => {
-	// y value is negative to convert from page top as 0 to center as 0
-	return Math.atan2(-(mousePosition.y - center.y), mousePosition.x - center.x) * 180 / Math.PI;
-};
-
 const App = () => {
 	const mapContainerOne = useRef(null);
 	const mapContainerTwo = useRef(null);
@@ -82,16 +71,9 @@ const App = () => {
 	const [mapTwoOpacity, setTwoOpacity] = useState(100);
 	const startCoordinatesOne = [45.53652083427686, -122.64854260371082];
 	const startCoordinatesTwo = [-37.80131284816989, 144.99433483493962];
-	const [angle, setAngle] = useState(0);
-	const [trackingAngle, isTrackingAngle] = useState(false);
 	// const zoom = 12;
 	// const [zoom, setZoom] = useState(9);
-	let center = {x: 0, y: 0};
 
-	// Maybe store center of maps when components are mounted?
-	// But then I might need to recalculate on resize.
-	// Maybe I should just handle overlap and opacity together
-	// instead of using z-index. And shift could drop top opacity?
 	// const toggleTop = () => {
 	// 	console.log('toggle: ', shiftPressed);
 	// 	if (shiftPressed) {
@@ -127,35 +109,6 @@ const App = () => {
 	// 	};
 	// }, []);
 
-	const startTrackingMouse = (event) => {
-		//currently assumes you're clicking inside the map
-		center = findCenter(event.target.getBoundingClientRect());
-		if (event.shiftKey) {
-			event.stopPropagation();
-			isTrackingAngle(true);
-		}
-		console.log(center, angle, event.clientX, event.clientY);
-	};
-	const handleMouseMove = (event) => {
-		if (event.shiftKey) {
-			event.stopPropagation();
-			setAngle(findAngle(center, {x: event.clientX, y: event.clientY}));
-		}
-	};
-	const stopTrackingMouse = () => {
-		isTrackingAngle(false);
-	};
-	useEffect(() => {
-		window.addEventListener('mousedown', startTrackingMouse, true);
-		window.addEventListener('mousemove', handleMouseMove, true);
-		window.addEventListener('mouseup', stopTrackingMouse, true);
-		return () => {
-			window.removeEventListener('mousemove', handleMouseMove);
-			window.removeEventListener('mousemove', handleMouseMove);
-			window.removeEventListener('mouseup', stopTrackingMouse);
-		};
-	}, []);
-
 	const setTopOne = () => setMapOneIsTop(true);
 	const setTopTwo = () => setMapOneIsTop(false);
 	const handleRangeOne = (e) => setOneOpacity(e.target.value);
@@ -164,7 +117,7 @@ const App = () => {
 
 	return (
 		<div>
-			<h2>Choose two locations or directions:</h2>
+			<h2>Hi there!</h2>
 			<form>
 				<h3>Opacity:</h3>
 				<OpacityRange name='Map 1' value={mapOneOpacity} onChange={handleRangeOne}/>
@@ -174,8 +127,8 @@ const App = () => {
 					<TopRadio map={mapOneIsTop} name="Map 1" onChange={setTopOne}/>
 					<TopRadio map={!mapOneIsTop} name="Map 2" onChange={setTopTwo}/>
 				</div>
+
 			</form>
-			<p>Angle: {angle}, Currently tracking: {trackingAngle ? 'yes' : 'no'}</p>
 			{/*<div className="sidebar">*/}
 			{/*  Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}*/}
 			{/*</div>*/}
